6b417013969b42f2aac3e9b1cf0ca9fcdc7dad95
Uncorv <113026029+Uncorv@users.noreply.github.com>
2023-01-16 02:25:03 +0700

    UI-Manager

---
+++ Assets/Scripts/UI/MainMenuWindow.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuWindow : Window
+{
+    private StartGameWindow startGameWindow;
+    private SettingsWindow settingsWindow;
+
+    public void Start()
+    {
+        startGameWindow = UIManager.Instance.GetWindow<StartGameWindow>();
+        settingsWindow = UIManager.Instance.GetWindow<SettingsWindow>();
+
+        startGameWindow.OnOpen += ChangeCurrentWindow;
+        settingsWindow.OnOpen += ChangeCurrentWindow;
+     
+    }
+    protected override void SelfOpen()
+    {
+        this.gameObject.SetActive(true);
+    }
+    protected override void SelfClose()
+    {
+        this.gameObject.SetActive(false);
+    }
+
+    public void StartGame()
+    {
+        startGameWindow.Open();
+    }
+
+    public void Settings()
+    {
+        settingsWindow.Open();
+    }
+
+    public void Exit()
+    {
+        Application.Quit();
+    }
+}
