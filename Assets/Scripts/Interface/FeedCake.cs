55d10788b7353dfd00963f640d40000f329b372e
Kontroman <ckontro@gmail.com>
2024-07-05 14:03:38 +0600

    cake now only affects to one customer

--- Assets/Scripts/Interface/FeedCake.cs
+++ Assets/Scripts/Interface/FeedCake.cs
@@ -1,4 +1,5 @@
 using Devotion.Scripts.Controllers;
+using Devotion.Scripts.Customers;
 using UnityEngine;
 
 namespace Devotion.Scripts.Interface
@@ -7,10 +8,25 @@ namespace Devotion.Scripts.Interface
     {
         public void ActiveCake()
         {
+            float minWaitTime = float.MaxValue;
+            Customer customerWithMinWaitTime = null;
+
             foreach (var customerPlace in CustomersController.Instance.CustomerPlaces)
             {
                 if (customerPlace.CurrentCustomer != null)
-                    customerPlace.CurrentCustomer.ResetTimer();
+                {
+                    float waitTime = customerPlace.CurrentCustomer.WaitTime;
+                    if (waitTime < minWaitTime)
+                    {
+                        minWaitTime = waitTime;
+                        customerWithMinWaitTime = customerPlace.CurrentCustomer;
+                    }
+                }
+            }
+
+            if (customerWithMinWaitTime != null)
+            {
+                customerWithMinWaitTime.ResetTimer();
             }
         }
     }
